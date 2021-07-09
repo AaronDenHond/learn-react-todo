6dcecb0e16b5de85fcc662a0162b3938e5e331d6
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-09 15:04:16 +0200

    working add button, onClick functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 //here we import the useState hook so we can manage state.
+//hooks can only be used in function components.
 import TodoList from "./components/TodoList";
+import { v4 as uuidv4 } from "uuid";
 function App() {
-  const [todos, setTodos] = useState(["Todo 1", "Todo 2", "Todo 3"]);
+  const [todos, setTodos] = useState([]);
   //the argument in useState is our default state, can be an empty array of todos, or multiple hardcoded ones,...
+
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") {
+      return;
+    }
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+    //clears input field after adding
+    console.log(todos.length);
+  }
   return (
     <>
       {/* we need this empty element (called a fragment, to return everything inside 1 element). we cant return more than 1 thing in a js function */}
       <TodoList todos={todos} />
       {/* first component we will make */}
-      <input type="text" />
-      <button>add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>add Todo</button>
       <button>clear completed</button>
       <div>0 left to do</div>
     </>
