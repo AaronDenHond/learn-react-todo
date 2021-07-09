21a541eb8c4751638f6ac038743920e4832b1d4c
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-09 12:14:57 +0200

    todoList and todos components, comments to understand

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
-
+import React, { useState } from "react";
+//here we import the useState hook so we can manage state.
+import TodoList from "./components/TodoList";
 function App() {
-  return null;
+  const [todos, setTodos] = useState(["Todo 1", "Todo 2", "Todo 3"]);
+  //the argument in useState is our default state, can be an empty array of todos, or multiple hardcoded ones,...
+  return (
+    <>
+      {/* we need this empty element (called a fragment, to return everything inside 1 element). we cant return more than 1 thing in a js function */}
+      <TodoList todos={todos} />
+      {/* first component we will make */}
+      <input type="text" />
+      <button>add Todo</button>
+      <button>clear completed</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 export default App;
+
+/* components have props(properties). They work just like html attributes. Above we added the props todos to TodoList and gave it a value
+ {} means we're passing a variable instead of like a string */
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+//create snippet with rfc + tab
+import Todo from "./Todo";
+
+export default function TodoList({ todos }) {
+  return todos.map((todo) => {
+    return <Todo key={todo} todo={todo} />;
+  });
+}
+
+/* we need unique ID's per list item, so that React doesnt have to rerender every Todo component in the list 
+whenever a component gets updated. we choose a key with value {todo} here since every todo name will be unique*/
